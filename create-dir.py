9c3a3cd451b606f74b285984c56976c5bc5b334a
MathiasOlsen <mathias.ao@live.dk>
2022-11-29 19:27:02 +0100

    Write function that create directories

---
+++ create-dir.py
@@ -0,0 +1,9 @@
+import os
+os.chdir('2015')
+file_exist=False
+for n in range(1,26):
+    dirmaker = f'day{n}'
+    try:
+        os.mkdir(dirmaker)
+    except FileExistsError:
+        print("Day already exists")
